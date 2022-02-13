23799d3b7a036eac23ea4206caaf9369dbbadce3
as6823917 <66206385+as6823917@users.noreply.github.com>
2022-02-13 15:39:21 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -338,7 +338,7 @@ async def bot_info(bot, message):
         ]
     await message.reply(text=f"{ABOUT}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
-@Client.on_message(filters.chat(GROUPS))
+@Client.on_message(filters.chat(AUTH_GROUPS))
 async def deleter(user, message):
     try:
        if message.from_user.id in ADMINS:
